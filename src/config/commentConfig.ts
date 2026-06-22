453e328613ae22b0179a30b13170b1a2974f4005
xiaye <xiaye@msn.com>
2026-06-22 16:24:24 +0800

    feat: 更新 Twikoo JS 文件地址至 1.7.13 版本

--- src/config/commentConfig.ts
+++ src/config/commentConfig.ts
@@ -13,9 +13,9 @@ export const commentConfig: CommentConfig = {
 		visitorCount: true,
 		// Twikoo JS 文件地址，支持 CDN 链接
 		// 中国推荐1: https://registry.npmmirror.com/twikoo/1.7.12/files/dist/twikoo.min.js
-		// 中国推荐2: https://s4.zstatic.net/npm/twikoo@1.7.12/dist/twikoo.min.js
-		// 国际推荐: https://cdn.jsdelivr.net/npm/twikoo@1.7.12/dist/twikoo.min.js
-		jsUrl: "https://cdn.jsdelivr.net/npm/twikoo@1.7.12/dist/twikoo.min.js",
+		// 中国推荐2: https://s4.zstatic.net/npm/twikoo@1.7.13/dist/twikoo.min.js
+		// 国际推荐: https://cdn.jsdelivr.net/npm/twikoo@1.7.13/dist/twikoo.min.js
+		jsUrl: "https://cdn.jsdelivr.net/npm/twikoo@1.7.13/dist/twikoo.min.js",
 		// Twikoo 自定义 CSS 文件地址，为空则不加载
 		cssUrl: "/assets/css/twikoo-custom.css",
 	},
